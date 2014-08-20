4f916be6823b53e29c80b97f770300d84b34e911
or3gano <allensam86@gmail.com>
2014-08-19 21:13:45 -0600

    Fixing Ace card functionality on bot php and javascript, making javascript print result in score div

--- index.php
+++ index.php
@@ -13,13 +13,10 @@
 <form>
     <label for="cards">Cards:</label>
     <input type="text" id="cards" name="cards" placeholder="Enter cards here">
-    <div id="ace_input">
-        <label for="ace">Please select a value for Ace:</label>
-        <input type="radio" id ="ace_one" name="ace" value="1">One(1)
-        <input type="radio" id ="ace_eleven" name="ace" value="11">Eleven(11)
+    <div id="button_group">
+        <button id="php_submit" type="submit">Run with PHP</button>
+        <button id="js_submit" type="reset">Run with JavaScript</button>
     </div>
-    <button id="php_submit" type="submit">Run with PHP</button>
-    <button id="js_submit" type="reset">Run with JavaScript</button>
 </form>
 
 
@@ -29,6 +26,7 @@
 
 <!--**--**--**--**--**--**      RUN WITH PHP      **--**--**--**--**--**-->
 
+<div id="score">
 <?php
 
 
@@ -37,6 +35,7 @@
 if (isset($_GET['cards'])) {
     $cards_array = str_split($_GET['cards']);  // turn card input into an array
     $num_variations = pow(2, count($cards_array));   // calculate the number of combinations possible
+    $comb_array = array();
     $sum_array = array();
     $ace = false;
 
@@ -46,34 +45,44 @@ if (isset($_GET['cards'])) {
         $cards_array[$i] = "10";
         }
         // convert ace to 0
-        elseif ($cards_array[$i] == "a") {
+        elseif (($cards_array[$i] == "a") || ($cards_array[$i] == "A")) {
             $cards_array[$i] = 0;
             $ace = true;
         }
     }
     
-    // get the sum of all possible combinations
+    // get the sum of all possible combinations and put them into an array
     for ($i = 0; $i < $num_variations; $i++) {
         $combination = 0;
         for ($j = 0; $j < count($cards_array); $j++) {
             if (pow(2, $j) & $i) {
-                if(!$ace) {$combination = $combination + (int)$cards_array[$j];}
-                
-                // re-enter ace as combinations of 1 and 11 if it was entered
-                if($ace) {
-                    for($k = 0; $k < 2; $k++) {
-                        if($k = 0) {$combination = $combination + 1;}
-                        if($k = 1) {$combination = $combination + 11; $ace = false;}
-                    }
-                }
+                $combination = $combination + (int)$cards_array[$j];
             }
-            echo $combination . "<br>";
         }
+        $comb_array[$i] = $combination;
+    }
+    
+    // re-enter ace as combinations of 1 and 11 if it was entered
+    for($k = 0; $k < count($comb_array); $k++) {
+        for($l = 0; $l < 3; $l++) {
+            if($l == 0) {
+                $sum_array[] = $comb_array[$k];
+            }
+            if(($l == 1) && ($ace)) {
+                $sum_array[] = $comb_array[$k] + 1;
+            }
+            if(($l == 2) && ($ace)) {
+                $sum_array[] = $comb_array[$k] + 11;
+            }
+        }
+    }
 
-        
-        // add the sums to $sum_array if they are less than or equal to 21
-        if ($combination <= 21) {
-            $sum_array[$i] = $combination;
+    // sets any values greater than 21 to 0
+    for($m = 0; $m < count($sum_array); $m++) {
+        if ($sum_array[$m] <= 21) {
+            $sum_array[$m] = $sum_array[$m];
+        } else {
+            $sum_array[$m] = 0;
         }
     }
     
@@ -84,7 +93,7 @@ if (isset($_GET['cards'])) {
 
 }
 ?>
-
+</div>
 
 
 
@@ -101,30 +110,21 @@ if (isset($_GET['cards'])) {
 $("#js_submit").click(function () {
     var cardsArray = document.getElementById('cards').value.split("");  // turn card input into an array
     var numVariations = Math.pow(2, cardsArray.length);   // calculate the number of combinations possible
+    var combArray = [];
     var sumArray = [];
     var ace = false;
     
-    // set ace value equal to selected radio value
-    if (document.getElementById("ace_one").checked) {
-        var ace = "1";
-    }
-    if (document.getElementById("ace_eleven").checked) {
-        var ace = "11";
-    }
-    
     // convert j,q,k to 10
     for (i = 0; i < cardsArray.length; i++) {
-        if ((cardsArray[i] == "j") || (cardsArray[i] == "q") || (cardsArray[i] == "k")) {    
-        cardsArray[i] = "10";
-        }
-        // convert ace to 0
-        else if (cardsArray[i] == "a") {
-            cardsArray[i] = 0;
+        if ((cardsArray[i] === "j") || (cardsArray[i] === "q") || (cardsArray[i] === "k")) {
+            cardsArray[i] = "10";
+        } else if ((cardsArray[i] === "a") || (cardsArray[i] === "A")) {
+            cardsArray[i] = 0;  // convert ace to 0
             ace = true;
         }
     }
     
-    // get the sum of all possible combinations
+    // get the sum of all possible combinations and put them into an array
     for (i = 0; i < numVariations; i++) {
         var combination = 0;
         for (j = 0; j < cardsArray.length; j++) {
@@ -132,25 +132,37 @@ $("#js_submit").click(function () {
                 combination = combination + parseInt(cardsArray[j]);
             }
         }
-        
-        // re-enter ace as combinations of 1 and 11 if it was entered
-        for(k = 0; k < 2; k++) {
-            if(ace) {
-                if(k = 0) {combination = combination + 1;}
-                if(k = 1) {combination = combination + 11;}
-            }
-        }
-        
-        // add the sums to sumArray if they are less than or equal to 21
-        if (combination <= 21) {
-            sumArray[i] = combination;
-        }
+        combArray[i] = combination;
     }
     
-    // get the highest number in the sumArray
+    // re-enter ace as combinations of 1 and 11 if it was entered
+    for (k = 0; k < combArray.length; k++) {
+        for (l = 0; l < 3; l++) {
+            if (l === 0) {
+                sumArray[sumArray.length] = combArray[k];
+            }
+            if ((l === 1) && (ace)) {
+                sumArray[sumArray.length] = combArray[k] + 1;
+            }
+            if ((l === 2) && (ace)) {
+                sumArray[sumArray.length] = combArray[k] + 11;
+            }
+        }
+    }
+
+    // sets any values greater than 21 to 0
+    for (m = 0; m < sumArray.length; m++) {
+        if (sumArray[m] <= 21) {
+            sumArray[m] = sumArray[m];
+        } else {
+            sumArray[m] = 0;
+        }
+    }    
+    
+    // get the highest number in the sumArray and print it in the score div
     sumArray = sumArray.filter(Boolean);    // filter out undefined values
     var num = Math.max.apply(null, sumArray);
-    alert("The highest winning combination is: " + num);
+    document.getElementById('score').innerHTML="The highest winning score is " + num;
     
 });
 
